7e667b59245ff0632cc5f648ef70c637b9e83b98
Bharat <bharat@wealthface.com>
2020-08-19 11:52:13 +0400

    some changes

--- js/test.js
+++ js/test.js
@@ -1,22 +1,3 @@
-function smallestDifference(arr1, arr2){
-  if(arr1.length === 0 || arr2.length === 0) {
-  	return -1;
-  }
-
-  let result = Number.MAX_SAFE_INTEGER;
-
-  for(let i = 0; i < arr1.length; i++) {
-  	for(let j = 0; j < arr2.length; j++) {
-  		if(Math.abs(arr1[i] - arr2[j]) < result) {
-  			result = Math.abs(arr1[i] - arr2[j])
-  		}
-  	}
-  }
-
-  return result;
-}
-
-const array2 = [1, 3, 15, 65];
-const array1 = [40, 25, 5];
-
-console.log(smallestDifference(array1, array2));
+const array1 = ['a', 'b', 'c', 'd', 'e'];
+console.log(array1.copyWithin(0, 3, 4));
+// expected output: Array ["d", "b", "c", "d", "e"]
