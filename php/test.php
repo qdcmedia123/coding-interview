eab365abf96f224a8a69920f1fc98e77efbd32eb
Bharat <bharat@wealthface.com>
2020-10-11 12:41:17 +0400

    Some php filter is added

--- php/test.php
+++ php/test.php
@@ -1,38 +1,13 @@
 <?php
-function maxProfit($price , $n, $k) {
-	// table to store results 
-	// of subproublems profit[t][i]
-	// stores maxiu profit using 
-	// atmost t transactions up to
-	// day i 
-	$profit[$k + 1][$n + 1] = 0;
+function filter_by_value($array, $index, $value) {
+	if(is_array($array) && count($array) > 0) {
+		foreach(array_keys($array) as $key) {
+			$tmp[$key] = $array[$key][$index];
 
-	// for day 0 you can't earcn money irrespective 
-	// how many times you trade 
-	for($i = 0; $i <= $k; $i++) {
-		$profit[$i][0] = 0;
-	}
-
-	for($j = 0; $j <= $n; $j++) {
-		$profit[0][$j] = 0;
-	}
-
-	// Fill the table in bott-up fahing 
-	$prevDiff = NULL;
-	for($i = 1; $i <= $k; $i++) {
-		for($j = 1; $j < $n; $j++) {
-			$prevDiff = max($prevDiff, $profit[$i-1][$j-1] - $price[$j -1]);
-			$profit[$i][$j] = max($profit[$i][$j - 1], $price[$j] + $prevDiff); 
+			if($temp[$key] == $value) {
+				$newArray[$key] = $array[$key];
+			}
 		}
 	}
-
-	return $profit[$k][$n -1];
+	return $newArray;
 }
-
-$k = 2; 
-    $price = array(5, 11, 3, 50,60, 90); 
-    $n = sizeof($price); 
-    echo "Maximum profit is: "
-         , maxProfit($price, $n, $k); 
-  
-// This code is contributed by nitin mittal 
